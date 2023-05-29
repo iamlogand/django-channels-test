6ebe87432ff4ed735e4b80747065289e8da82fa7
Logan Davidson <iamlogandavidson@gmail.com>
2023-05-29 22:43:57 +0100

    Add a chat room, consumer and Redis channel layer

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -125,6 +125,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# Daphne
+# Channels
 
 ASGI_APPLICATION = "chatapp.asgi.application"
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
